9d6c866471ae2764b1396df0dcb6bb1583fcf227
Filip Brebera <breberafilip@icloud.com>
2020-12-05 22:31:53 +0100

    fix: Netlify form (#15)
    * feat: added disclaimer + consts
    
    * fix: remove console.log
    
    * feat: merged to netlify, static assets
    
    * fix: added missing slashes
    
    * fix: wasm cache control for 3 days
    
    * feat: fix?
    
    * Revert "Merge branch 'production' into development"
    
    This reverts commit 7da2937df6f9270e9ab95b62cf9e2695e7995dbe, reversing
    changes made to e1276fa1d33a74ba6ffdec815062c6af7158e538.
    
    * feat: netlify form
    
    * fix: fix conflicts
    
    * feat: cache js
    
    * fix: added form-name input
    
    * fix: set method to post

--- src/components/contact-form/contact-form.tsx
+++ src/components/contact-form/contact-form.tsx
@@ -9,7 +9,7 @@ const ContactForm = () => {
   getPrice.observe_((change) => setPrice(change.newValue));
   return (
     <div class={module.contactform}>
-      <form name="contact">
+      <form name="contact" method="post">
         <input type="hidden" name="form-name" value="contact" />
         <h2>Zaujala Vás naše nabídka?</h2>
         <p>Zanechte nám na sebe kontakt a my se Vám ozveme.</p>
