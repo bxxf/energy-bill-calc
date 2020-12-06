5d28639376ac6bf73b4f671321f27722bfeb76fa
Filip Brebera <breberaf@gmail.com>
2020-12-06 20:20:43 +0100

    fix: fixed placeholders

--- src/components/contact-form/contact-form.tsx
+++ src/components/contact-form/contact-form.tsx
@@ -16,11 +16,19 @@ const ContactForm = () => {
   observe(consumption, 'gas', (change: any) => setGas(change.newValue));
   return (
     <div class={module.contact}>
-      <form name="contact" method="post" class={module.contact__form}>
+      <form
+        name="contact"
+        method="post"
+        class={module.contact__form}
+        aria-label="Kontaktní formulář"
+      >
         <input type="hidden" name="form-name" value="contact" />
         <h2 class={module.contact__form__header}>Zaujala Vás naše nabídka?</h2>
         <p class={module.contact__form__description}>
           Zanechte nám na sebe kontakt a my se Vám ozveme.
+        </p>
+        <p class={module.contact__form__disclaimer}>
+          Položky označené * jsou <span id="required">povinné</span>.
         </p>
 
         <textarea name="electricity" hidden required>
@@ -34,36 +42,43 @@ const ContactForm = () => {
         </textarea>
 
         <div class={module['contact__form__input-group']}>
-          <label for="name">*Vaše jméno:</label>
+          <label for="name" id="name-label">
+            <span aria-labelledby="required">*</span>Zadejte Vaše jméno:
+          </label>
           <input
+            aria-describedby="name-label"
             class={module['contact__form__input-group__input']}
             type="text"
             name="name"
-            placeholder="Karel Novák"
+            placeholder="Příklad: Karel Novák"
             id="name"
             required
           />
         </div>
         <div class={module['contact__form__input-group']}>
-          <label for="email">*Váš email:</label>
+          <label for="email" id="email-label">
+            <span aria-labelledby="required">*</span>Zadejte Váš email:
+          </label>
           <input
             class={module['contact__form__input-group__input']}
             type="email"
-            placeholder="karel.novak@email.cz"
+            aria-describedby="email-label"
+            placeholder="Příklad: karel.novak@email.cz"
             name="email"
             id="email"
             required
           />
         </div>
         <div class={module['contact__form__input-group']}>
-          <label for="message">
-            Máte nějaké speciální požadavky? (nepovinné)
+          <label for="message" id="message-label">
+            Pokud máte nějaké speciální požadavky, zadejte je:
           </label>
           <textarea
             name="message"
             id="message"
+            aria-describedby="message-label"
             class={module['contact__form__input-group__input']}
-            placeholder="Dobrý den, chtěl bych se zeptat na..."
+            placeholder="Příklad: Dobrý den, chtěl bych se zeptat na..."
           />
         </div>
         <input
